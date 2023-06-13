fd3315ea83ff672a58e6ae602ff6917e14aac2e1
raffaelecapaldo <raffaelecpl@gmail.com>
2023-06-13 14:38:54 +0200

    Scaffolding Vue

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'App',
+    data() {
+      return {
+        title:'Hello World'
+      }
+    },
+    methods: {
+
+    }
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
