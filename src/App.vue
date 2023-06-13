af64751975040a5bd20ccb88ba86469e8ace9916
raffaelecapaldo <raffaelecpl@gmail.com>
2023-06-13 14:53:18 +0200

    Added api request /w console.log

--- src/App.vue
+++ src/App.vue
@@ -5,15 +5,25 @@
 </template>
 
 <script>
+import axios from 'axios';
   export default {
     name: 'App',
     data() {
       return {
-        title:'Hello World'
+        title:'Hello World',
+        apiUrl: 'http://localhost:8000/api'
       }
     },
     methods: {
-
+      getProjects() {
+        axios.get(`${this.apiUrl}/projects`).then((res) => {
+          const projects = res.data.results.data;
+          console.log(projects);
+        })
+      }
+    },
+    mounted() {
+      this.getProjects();
     }
     
   }
