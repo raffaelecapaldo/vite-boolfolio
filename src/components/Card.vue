75130200340a77a3971d6ed9d3e181973a1a47c3
raffaelecapaldo <raffaelecpl@gmail.com>
2023-06-14 17:14:00 +0200

    Added router link for cards

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,74 +1,78 @@
 <template>
-        <div class="col-12 pe-1 col-sm-6 col-md-6 col-lg-4 col-xxl-3 d-flex justify-content-center mb-3">
-          <div class="card" style="width: 22rem;">
+    <div class="col-12 pe-1 col-sm-6 col-md-6 col-lg-4 col-xxl-3 d-flex justify-content-center mb-3">
+        <div class="card" style="width: 22rem;">
             <div class="monitor position-relative">
-              <img class="card-img-top  matrix-monitor img-fluid " src="images/matrix.png" alt="">
-  
-              <div class="project-image position-absolute">
-                <img :src="project.image_url" class="card-img-top project-image img-fluid position-absolute" alt="...">
-              </div>
-          </div>
+                <img class="card-img-top  matrix-monitor img-fluid " src="images/matrix.png" alt="">
+
+                <div class="project-image position-absolute">
+                    <img :src="project.image_url" class="card-img-top project-image img-fluid position-absolute" alt="...">
+                </div>
+            </div>
             <div class="card-body">
-              <h5 class="card-title">{{ project.name }}</h5>
-              <MatrixDescription :description="project.description"/>
-              <div class="languages mb-2">
-                <span v-for="language in project.languages" :style="{ 'background-color': language.badge_color }"
-                  class="badge me-1">{{ language.name }}</span>
-              </div >
-              <div class="buttons d-flex justify-content-center">
-              <a :href="project.repo_url" class="btn btn-primary">Visualizza</a>
+                <h5 class="card-title">{{ project.name }}</h5>
+                <MatrixDescription :description="project.description" />
+                <div class="languages mb-2">
+                    <span v-for="language in project.languages" :style="{ 'background-color': language.badge_color }"
+                        class="badge me-1">{{ language.name }}</span>
+                </div>
+                <div class="buttons d-flex justify-content-center">
+                    <router-link :to="{ name: 'Project', params: { slug: project.slug } }" class="btn btn-primary">
+                        Visualizza
+                    </router-link>
+                </div>
             </div>
-            </div>
-            
-          </div>
-          
+
         </div>
+
+    </div>
 </template>
 
 <script>
-  import MatrixDescription from '../components/MatrixDescription.vue'
-    export default {
-        name: 'Card',
-        props: ['project'],
-        components: {
-            MatrixDescription
-        }
-        
+import MatrixDescription from '../components/MatrixDescription.vue'
+export default {
+    name: 'Card',
+    props: ['project'],
+    components: {
+        MatrixDescription
     }
+
+}
 </script>
 
 <style lang="scss" scoped>
 @use '../assets/styles/partials/variables' as *;
-  
-  .card {
+
+.card {
     background-color: $darkgreen;
-    color:$lightgreen;
-  .card-title {
-    color:white;
-  }
+    color: $lightgreen;
+
+    .card-title {
+        color: white;
+    }
+
     .btn {
-      width: 200px;
-      background-color: $buttonscolor;
-      border-color:$bgcolor;
+        width: 200px;
+        background-color: $buttonscolor;
+        border-color: $bgcolor;
     }
+
     .project-image {
-      top: 8px;
-      left: 11px;
-      width: 94%;
-      height: 94%;
+        top: 8px;
+        left: 11px;
+        width: 94%;
+        height: 94%;
     }
-  }
-  
-  .monitor {
+}
+
+.monitor {
     width: 100%;
-  }
-  
-  @media screen and (max-width:624px) and (min-width:576px) {
+}
+
+@media screen and (max-width:624px) and (min-width:576px) {
     .card {
-    .project-image
-    {
-      width: 92%;
-      height: 92%;
+        .project-image {
+            width: 92%;
+            height: 92%;
+        }
     }
-  }}
-</style>
+}</style>
