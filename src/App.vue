ec9201ea4418f5975578050fbb926b2f85ec4a2f
raffaelecapaldo <raffaelecpl@gmail.com>
2023-06-15 14:00:57 +0200

    Added footer

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,16 @@
   <main>
      <router-view></router-view>
     </main>
+    <Footer/>
 </template>
 
 <script>
 import Header from './components/Header.vue';
+import Footer from './components/Footer.vue';
   export default {
     components: {
       Header,
+      Footer,
     }
     
   }
---
+++ src/components/Footer.vue
@@ -0,0 +1,23 @@
+<template>
+   <footer>
+    <div class="py-3 d-flex justify-content-center">
+        <p class="m-0">Made with <span class="text-danger px-2">&hearts;</span> by Raffaele</p>
+    </div>
+   </footer>
+</template>
+
+<script>
+    export default {
+        name: 'Footer',
+
+    }
+</script>
+
+<style lang="scss" scoped>
+footer {
+    background-color: #004238;
+    p {
+        font-size:2rem;
+    }
+}
+</style>
